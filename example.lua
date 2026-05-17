7dfd62f70c6cb5fb673cf8ffb7a36354f7ec0402
nivalos0 <isaiahepperson80@gmail.com>
2026-05-17 19:12:46 -0400

    Update example.lua

--- example.lua
+++ example.lua
@@ -1,6 +1,6 @@
 debugX = true
 
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/nivalos0/Rayfield-Plus/main/source.lua'))()
 
 local Window = Rayfield:CreateWindow({
    Name = "Rayfield Example Window",
